602c4b269a5eb5c6e0d689718b23d656eba2c71f
Isha Dabas <83919888+IshaDabas@users.noreply.github.com>
2025-05-11 13:44:16 -0400

    update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import pdf from "../../Assets/Isha_Dabas.pdf";
+import pdf from "../../Assets/Isha_Dabas_Res.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
